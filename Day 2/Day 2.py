f5e46aece6eab1fbab0c692b2957c6da6923cabf
kso <so.kathy@gmail.com>
2018-12-05 14:47:52 -0800

    Update to Part 2 Solution

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -3,29 +3,29 @@
 # set = set(list) = unique list
 
 #to open file
-#f = open(r"C:\Users\kso\PycharmProjects\AdventOfCode2018\Day 2\Day2Input.txt", "r")
-#
-# #PART 1
-#
-# # counter
-# twocount = 0
-# threecount = 0
-#
-# for line in f:
-#     #print(line.replace("\n", ""))
-#     count = {}
-#     for s in line:
-#         count[s] = line.count(s)
-#     if 2 in count.values():
-#         twocount += 1
-#     if 3 in count.values():
-#         threecount +=1
-#     else:
-#         pass
-#
-# print ('two count:', twocount)
-# print ('three count:', threecount)
-# print (twocount*threecount)
+f = open(r"C:\Users\kso\PycharmProjects\AdventOfCode2018\Day 2\Day2Input.txt", "r")
+
+#PART 1
+
+# counter
+twocount = 0
+threecount = 0
+
+for line in f:
+    #print(line.replace("\n", ""))
+    count = {}
+    for s in line:
+        count[s] = line.count(s)
+    if 2 in count.values():
+        twocount += 1
+    if 3 in count.values():
+        threecount +=1
+    else:
+        pass
+
+print ('two count:', twocount)
+print ('three count:', threecount)
+print (twocount*threecount)
 
 #PART 2
 
@@ -51,9 +51,12 @@ for wordone in ids:
         if count == 1:#set if statement outside of loop, because once end loop, then can check what total count is
             toggle = 1 #set toggle to 1 to break word one-loop
             print(wordoneclean, wordtwoclean) #the two words that only have 1 letter different
+            common = ""
+            for k,c in enumerate(wordoneclean): #prints out every letter into the string
+                if wordoneclean[k] == wordtwoclean[k]:
+                    common += c
+            print(common)
             break #to get out of word two-loop
 
     if toggle == 1:
         break
-
-#need to have it read and produce all the letters that are the same
