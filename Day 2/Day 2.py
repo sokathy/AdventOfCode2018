67ee5990bd3037a09e14c9dc54b49d0eb55ba6aa
kso <so.kathy@gmail.com>
2018-12-04 19:10:57 -0800

    Added Part 2 solution

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -3,24 +3,57 @@
 # set = set(list) = unique list
 
 #to open file
-f = open(r"C:\Users\kso\PycharmProjects\AdventOfCode2018\Day 2\Day2Input.txt", "r")
+#f = open(r"C:\Users\kso\PycharmProjects\AdventOfCode2018\Day 2\Day2Input.txt", "r")
+#
+# #PART 1
+#
+# # counter
+# twocount = 0
+# threecount = 0
+#
+# for line in f:
+#     #print(line.replace("\n", ""))
+#     count = {}
+#     for s in line:
+#         count[s] = line.count(s)
+#     if 2 in count.values():
+#         twocount += 1
+#     if 3 in count.values():
+#         threecount +=1
+#     else:
+#         pass
+#
+# print ('two count:', twocount)
+# print ('three count:', threecount)
+# print (twocount*threecount)
 
-# counter
-twocount = 0
-threecount = 0
+#PART 2
 
-for line in f:
-    #print(line.replace("\n", ""))
-    count = {}
-    for s in line:
-        count[s] = line.count(s)
-    if 2 in count.values():
-        twocount += 1
-    if 3 in count.values():
-        threecount +=1
-    else:
-        pass
+file = open(r"C:\Users\kso\PycharmProjects\AdventOfCode2018\Day 2\Day2Input.txt", "r")
+ids = file.readlines()
 
-print ('two count:', twocount)
-print ('three count:', threecount)
-print (twocount*threecount)
+toggle = 0 #use this as toggle to get out of word one-loop
+
+for wordone in ids:
+
+    wordoneclean = wordone.replace("\n", "") #to take out the space at end of lines
+
+    for wordtwo in ids: #for every word in list to compare to (p is word comparing to)
+
+        wordtwoclean = wordtwo.replace("\n", "") #to take out the space at end of lines
+
+        count = 0 #set count here before it loops through letters
+
+        for k, c in enumerate(wordtwoclean):
+            if wordoneclean[k] != wordtwoclean[k]:
+                count += 1
+
+        if count == 1:#set if statement outside of loop, because once end loop, then can check what total count is
+            toggle = 1 #set toggle to 1 to break word one-loop
+            print(wordoneclean, wordtwoclean) #the two words that only have 1 letter different
+            break #to get out of word two-loop
+
+    if toggle == 1:
+        break
+
+#need to have it read and produce all the letters that are the same
